a6d684d7f4d775793ac343725ded9f19cb8f26bf
James Wang <wangjames64000@gmail.com>
2023-04-24 16:49:48 +0200

    Changement de GUI

--- core/src/fr/supdevinci/game/Main.java
+++ core/src/fr/supdevinci/game/Main.java
@@ -3,23 +3,29 @@ package fr.supdevinci.game;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class Main extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
+	TextureRegion spritesheet;
+	TextureRegion[][] frames;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		img = new Texture(".././assets/game-assets/gui/base free.png");
+		int frameWidth = spritesheet.getRegionWidth() / 8;
+		int frameHeight = spritesheet.getRegionHeight() / 24;
+		frames = spritesheet.split(frameWidth, frameHeight);
 	}
 
 	@Override
 	public void render () {
 		ScreenUtils.clear(1, 0, 0, 1);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(img, 0, 0, 300,300);
 		batch.end();
 	}
 	
